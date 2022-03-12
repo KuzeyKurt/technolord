9d390bfd70c1fe67a9173432aa2d214c0f5bde23
KuzeyKurt <72691339+KuzeyKurt@users.noreply.github.com>
2022-03-12 23:22:53 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<DOCTYPE html>
+
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <title> TechnoLord </title>
+     <link href="/css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <header>
+        <div id="logo">
+
+        <a href="/" title = "Перейти на главную"><span> TECHNO</span>LORD </a>
+        </div>
+
+        <div id="menuHead">
+           <div style="margin-right: 5%"> О нас</div><div>Обратная связь</div>
+        </div>
+
+        <div id="regAuth">
+            Регистрация | Авторизация
+        </div>
+        
+    </header>
+</body>
+</html>
