e881a437e07e5a9a123961fd79f8995ef46dd5cd
KuzeyKurt <72691339+KuzeyKurt@users.noreply.github.com>
2022-03-20 15:05:45 +0300

    Errors corrected, every links works correctly

--- index.php
+++ index.php
@@ -7,42 +7,46 @@
     <title> TechnoLord </title>
      <link href="/css/style.css" rel="stylesheet" type="text/css">
 </head>
-
 <body>
     <header>
         <div id="logo">
-
         <a href="/" title = "Перейти на главную"><span> TECHNO</span>LORD </a>
         </div>
 
         <div id="about">
-            <a href="about.php">
+            <a href="/about.php">
            О НАС
+</a>
         </div>
 
         <div id="lib">
-            БИБЛИОТЕКА
+            <a href="/library.php">
+            БИБЛИОТЕКА </a>
         </div>
 
         <div id="device">
+            <a href="/device.php">
             УСТРОЙСТВА
+</a>
         </div>
 
         <div id="learn">
+            <a href="/learn.php">
             РАСПИСАНИЕ
+</a>
         </div>
 
         <div id="auth">
+            <a href="auth.php">
             ВОЙТИ
+</a>
         </div>
 
         <div id="reg">
+            <a href="reg.php">
             РЕГИСТРАЦИЯ
+</a>
         </div>
-
-
-
-
         
     </header>
 </body>
