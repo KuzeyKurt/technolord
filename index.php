cb960ddfe62bc0f91ac4900f054b3197bbb606fb
KuzeyKurt <72691339+KuzeyKurt@users.noreply.github.com>
2022-03-27 04:22:01 +0300

    footer added

--- index.php
+++ index.php
@@ -49,5 +49,25 @@
         </div>
         
     </header>
+
+    <div id="wrapper">
+        <div id="leftCol">
+        </div>
+
+
+        <div id="rightCol">
+        </div>
+    </div>
+
+    <footer>
+        <div id="social">
+           <a href="http://youtube.com" title="Ютюб"> <img src="/img/youtube_b.png" alt="YouTube">
+        </a>
+        </div>
+
+        <div id="rights">
+            Все права защищены &copy; <?php echo date ('Y')?> 
+        </div>
+    </footer>
 </body>
 </html>
